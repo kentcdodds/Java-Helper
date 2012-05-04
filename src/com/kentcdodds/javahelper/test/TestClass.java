6dfe1cc0f3c6a31ed83a1eff75d6836e76b3efcf
Kent <me@kentcdodds.com>
2012-05-03 21:09:42 -0600

    Other commit

--- src/com/kentcdodds/javahelper/test/TestClass.java
+++ src/com/kentcdodds/javahelper/test/TestClass.java
@@ -22,7 +22,6 @@ public class TestClass {
 
   public static void main(String[] args) throws Exception {
     setStuff();
-    resizeImage();
     //    List<File>[] replaced = IOHelper.replaceInAllFiles(new File("C:\\Users\\Kent\\Documents\\My Dropbox\\Work\\MGF\\NetBeansProjects\\MyVideoFacilitator\\src"),
     //            5, "org/mvf/resources/", "/org/mvf/resources/");
     //    PrinterHelper.setInstancePrint(true);
