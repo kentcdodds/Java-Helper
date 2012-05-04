654ec8ec61e2bdff008c02566d55a475c2b66c05
kentcdodds <kentcdodds@PBDHB61.ldschurch.org>
2012-05-04 11:49:37 -0600

    added for email functionality

---
+++ src/model/Email.java
@@ -0,0 +1,192 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Part;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.util.ByteArrayDataSource;
+
+/**
+ *
+ * @author kentcdodds
+ */
+public class Email {
+
+  private String from;
+  private List<String> to;
+  private List<String> cc;
+  private List<String> bcc;
+  private String subject = "";
+  private String body = "";
+  private List<MimeBodyPart> bodyParts = new ArrayList<>();
+
+  //<editor-fold defaultstate="collapsed" desc="Constructors">
+  /**
+   * Constructor: Don't forget to set variables!
+   */
+  public Email() {
+  }
+
+  /**
+   * Constructor: Use for e-mails without attachments
+   *
+   * @throws MessagingException when trying to add the body to the message
+   */
+  public Email(String from, List<String> to, List<String> cc, List<String> bcc, String subject, String body) throws MessagingException {
+    this.from = from;
+    this.to = to;
+    this.cc = cc;
+    this.bcc = bcc;
+    this.subject = subject;
+    this.body = body;
+  }
+
+  /**
+   * Constructor: Use for e-mails with attachments (the message should be included in the body parts)
+   */
+  public Email(String from, List<String> to, List<String> cc, List<String> bcc, String subject, String body, List<MimeBodyPart> bodyParts) {
+    this.from = from;
+    this.to = to;
+    this.cc = cc;
+    this.bcc = bcc;
+    this.subject = subject;
+    this.bodyParts = bodyParts;
+  }
+  //</editor-fold>
+
+  /**
+   * Adds the given attachment as a body part. First checks whether the attachment has bytes already. This allows you to
+   * give an email attachment which has bytes in memory and doesn't have an actual file. If the bytes are null it reads
+   * the bytes from the file. If the file is null no attachment is added.
+   *
+   * @param attachment
+   * @return whether the attachment was successfully added to the bodyParts list
+   */
+  public boolean addEmailAttachment(EmailAttachment attachment) throws MessagingException {
+    DataSource source;
+    if (attachment.getFileBytes() != null) {
+      source = new ByteArrayDataSource(attachment.getFileBytes(), Message.ATTACHMENT);
+    } else if (attachment.getFile() != null) {
+      source = new FileDataSource(attachment.getFile()) {
+
+        @Override
+        public String getContentType() {
+          return "application/octet-stream";
+        }
+      };
+    } else {
+      return false;
+    }
+    MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+    messageBodyPart.setDataHandler(new DataHandler(source));
+    messageBodyPart.setFileName(attachment.getFullFilename());
+    bodyParts.add(messageBodyPart);
+    return true;
+  }
+
+  //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
+  /**
+   * @return the from
+   */
+  public String getFrom() {
+    return from;
+  }
+
+  /**
+   * @param from the from to set
+   */
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  /**
+   * @return the to
+   */
+  public List<String> getTo() {
+    return to;
+  }
+
+  /**
+   * @param to the to to set
+   */
+  public void setTo(List<String> to) {
+    this.to = to;
+  }
+
+  /**
+   * @return the cc
+   */
+  public List<String> getCc() {
+    return cc;
+  }
+
+  /**
+   * @param cc the cc to set
+   */
+  public void setCc(List<String> cc) {
+    this.cc = cc;
+  }
+
+  /**
+   * @return the bcc
+   */
+  public List<String> getBcc() {
+    return bcc;
+  }
+
+  /**
+   * @param bcc the bcc to set
+   */
+  public void setBcc(List<String> bcc) {
+    this.bcc = bcc;
+  }
+
+  /**
+   * @return the subject
+   */
+  public String getSubject() {
+    return subject;
+  }
+
+  /**
+   * @param subject the subject to set
+   */
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  /**
+   * @return the bodyParts
+   */
+  public List<MimeBodyPart> getBodyParts() {
+    return bodyParts;
+  }
+
+  /**
+   * @param bodyParts the bodyParts to set
+   */
+  public void setBodyParts(List<MimeBodyPart> bodyParts) {
+    this.bodyParts = bodyParts;
+  }
+
+  /**
+   * @return the body
+   */
+  public String getBody() {
+    return body;
+  }
+
+  /**
+   * @param body the body to set
+   */
+  public void setBody(String body) {
+    this.body = body;
+  }
+  //</editor-fold>
+}
---
+++ src/model/EmailAttachment.java
@@ -0,0 +1,114 @@
+package model;
+
+import com.kentcdodds.javahelper.helpers.IOHelper;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ *
+ * @author Kent Dodds
+ */
+public class EmailAttachment {
+
+  private File file;
+  private byte[] fileBytes;
+  private String fileName;
+  private String extension;
+  private String fullFilename;
+
+  /**
+   * Don't forget to set filetype, filename, and file
+   */
+  public EmailAttachment() {
+  }
+
+  /**
+   * Don't forget to set filetype, filename, and file
+   */
+  public EmailAttachment(File file, byte[] fileBytes) {
+    this.file = file;
+    this.fileBytes = fileBytes;
+  }
+
+  /**
+   * Gets the filename of the file ("document.doc" returns "document")
+   *
+   * @return the filename of the file
+   */
+  public String getFileName() {
+    if (fileName == null) {
+      generateFileInfo();
+    }
+    return fileName;
+  }
+
+  public String getExtension() {
+    if (extension == null) {
+      generateFileInfo();
+    }
+    return extension;
+  }
+
+  public String getFullFilename() {
+    if (fileName == null
+            || extension == null) {
+      generateFileInfo();
+    }
+    if (fullFilename == null) {
+      fullFilename = fileName + "." + extension;
+    }
+    return fullFilename;
+  }
+
+  /**
+   * Generates the file info
+   */
+  private void generateFileInfo() {
+    if (file != null) {
+      String name = file.getName();
+      int dot = name.lastIndexOf(".");
+      fileName = name.substring(0, dot);
+      extension = name.substring(dot + 1); // + 1 to get rid of the dot
+    }
+  }
+
+  /**
+   * Generates the bytes of the file from the current file. WARNING: This will not work if the file is greater than 2 MB
+   * because that's the maximum size of an array
+   *
+   * @return the file bytes
+   */
+  public byte[] generateFileBytes() throws FileNotFoundException, IOException {
+    this.fileBytes = IOHelper.getFileBytes(file);
+    return getFileBytes();
+  }
+
+  /**
+   * @return the file
+   */
+  public File getFile() {
+    return file;
+  }
+
+  /**
+   * @param file the file to set
+   */
+  public void setFile(File file) {
+    this.file = file;
+  }
+
+  /**
+   * @return the fileBytes
+   */
+  public byte[] getFileBytes() {
+    return fileBytes;
+  }
+
+  /**
+   * @param fileBytes the fileBytes to set
+   */
+  public void setFileBytes(byte[] fileBytes) {
+    this.fileBytes = fileBytes;
+  }
+}
