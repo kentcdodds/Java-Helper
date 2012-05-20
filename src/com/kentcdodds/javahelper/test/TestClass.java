72ce4e50e428b43a51ad326a7e8efcb692f635b1
kentcdodds <kentcdodds@PBDHB61.ldschurch.org>
2012-05-20 15:52:38 -0600

    Added progressWheel test

--- src/com/kentcdodds/javahelper/test/TestClass.java
+++ src/com/kentcdodds/javahelper/test/TestClass.java
@@ -5,21 +5,25 @@ import com.kentcdodds.javahelper.model.Email;
 import com.kentcdodds.javahelper.model.EmailAttachment;
 import com.kentcdodds.javahelper.model.HelperFile;
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.imageio.ImageIO;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JWindow;
+import org.apache.commons.lang3.math.NumberUtils;
 
 /**
  *
@@ -28,28 +32,147 @@ import javax.swing.JLabel;
 public class TestClass {
 
   public static String testImageLocation = "/com/kentcdodds/javahelper/resources/iSayHiGuy.jpg";
+  public static String progressImageUrl = "http://c96265.r65.cf3.rackcdn.com/indicator_medium.gif";
   public static File ioPlaygroundDir = new File(IOHelper.homeDir + "\\IOPlayground\\");
 
   public static void main(String[] args) throws Exception {
     setStuff();
-    unzip();
+    progressImage();
     System.exit(0);
+  }
+  
+  public static void progressImage() throws IOException, InterruptedException {
+    URL url = new URL(progressImageUrl);
+    java.awt.Image image = Toolkit.getDefaultToolkit().createImage(url);
+    JWindow window = SwingHelper.getProgressWheelWindow(new ImageIcon(image));
+    window.setVisible(true);
+    Thread.sleep(3000);
+    window.setVisible(false);
+  }
+
+  public static void parseNumber() throws Exception {
+    long numberHelperTotal = 0;
+    long numberUtilsTotal = 0;
+    long regExTotal = 0;
+    long bruteForceTotal = 0;
+    long scannerTotal = 0;
+    int iterations = 5;
+    for (int i = 0; i < iterations; i++) {
+      long numberHelper = 0;
+      long numberUtils = 0;
+      long regEx = 0;
+      long bruteForce = 0;
+      long scanner = 0;
+      for (int j = 0; j < 99999; j++) {
+        long start;
+        long end;
+        Random rand = new Random();
+        String string = ((rand.nextBoolean()) ? "" : "-") + String.valueOf(rand.nextDouble() * j);
+        //NumberHelper
+        start = System.nanoTime();
+        NumberHelper.isValidNumber(double.class, string);
+        end = System.nanoTime();
+        numberHelper += end - start;
+
+        //NumberUtils
+        start = System.nanoTime();
+        NumberUtils.isNumber(string);
+        end = System.nanoTime();
+        numberUtils += end - start;
+
+        //RegEx
+        start = System.nanoTime();
+        Pattern p = Pattern.compile("^[-+]?[0-9]*\\.?[0-9]+$");
+        Matcher m = p.matcher(string);
+        if (m.matches()) {
+          Double.parseDouble(string);
+        }
+        end = System.nanoTime();
+        regEx += end - start;
+
+        //Brute Force (not international support) and messy support for E and negatives
+        //This is not the way to do it...
+        start = System.nanoTime();
+        int decimalpoints = 0;
+        for (char c : string.toCharArray()) {
+          if (Character.isDigit(c)) {
+            continue;
+          }
+          if (c != '.') {
+            if (c == '-' || c == 'E') {
+              decimalpoints--;
+            } else {
+              //return false
+              //because it should never return false in this test, I will throw an exception here if it does.
+              throw new Exception("Brute Force returned false! It doesn't work! The character is " + c + " Here's the number: " + string);
+            }
+          }
+          if (decimalpoints > 0) {
+            //return false
+            //because it should never return false in this test, I will throw an exception here if it does.
+            throw new Exception("Brute Force returned false! It doesn't work! The character is " + c + " Here's the number: " + string);
+          }
+          decimalpoints++;
+        }
+        end = System.nanoTime();
+        bruteForce += end - start;
+
+        //Scanner
+        start = System.nanoTime();
+        Scanner scanNumber = new Scanner(string);
+        if (scanNumber.hasNextDouble()) {//check if the next chars are integer
+          //return true;
+        } else {
+          //return false;
+          //because it should never return false in this test, I will throw an exception here if it does.
+          throw new Exception("Scanner returned false! It doesn't work! Here's the number: " + string);
+        }
+        end = System.nanoTime();
+        scanner += end - start;
+
+        //Increase averages
+        //For debug:
+        //System.out.println("String: " + string);
+        //System.out.println("NumberHelper: " + numberHelper);
+        //System.out.println("NumberUtils: " + numberUtils);
+        //System.out.println("RegEx: " + regEx);
+        //System.out.println("Brute Force: " + bruteForce);
+        //System.out.println("Scanner: " + scanner);
+      }
+      numberHelperTotal += numberHelper;
+      numberUtilsTotal += numberUtils;
+      regExTotal += regEx;
+      bruteForceTotal += bruteForce;
+      scannerTotal += scanner;
+    }
+
+    long numberHelperAvg = numberHelperTotal / iterations;
+    long numberUtilsAvg = numberUtilsTotal / iterations;
+    long regExAvg = regExTotal / iterations;
+    long bruteForceAvg = bruteForceTotal / iterations;
+    long scannerAvg = scannerTotal / iterations;
+    System.out.println("NumberHelper: " + (numberHelperAvg / 1000000) + " milliseconds -> " + (numberHelperAvg / 1000000000) + " seconds");
+    System.out.println("NumberUtils: " + (numberUtilsAvg / 1000000) + " milliseconds -> " + (numberUtilsAvg / 1000000000) + " seconds");
+    System.out.println("RegEx: " + (regExAvg / 1000000) + " milliseconds -> " + (regExAvg / 1000000000) + " seconds");
+    System.out.println("Brute Force: " + (bruteForceAvg / 1000000) + " milliseconds -> " + (bruteForceAvg / 1000000000) + " seconds");
+    System.out.println("Scanner: " + (scannerAvg / 1000000) + " milliseconds -> " + (scannerAvg / 1000000000) + " seconds");
   }
 
   /**
    * Tests for unzip methods
+   *
    * @throws FileNotFoundException
-   * @throws IOException 
+   * @throws IOException
    */
   public static void unzip() throws FileNotFoundException, IOException, Exception {
 //    File zipped = new File(ioPlaygroundDir, "unzip-haha.zip");
 //    File zipOutput = new File(ioPlaygroundDir, "unzip-hahe\\");
 //    zipOutput.mkdir();
 //    IOHelper.unzipFiles(zipped, zipOutput);
-    
-    
+
+
     HelperFile hFile1 = new HelperFile(new File(ioPlaygroundDir, "wiki1.txt"));
-    HelperFile hFile2 = new HelperFile(new File(ioPlaygroundDir,  "wiki2.txt"));
+    HelperFile hFile2 = new HelperFile(new File(ioPlaygroundDir, "wiki2.txt"));
     byte[] zipFiles = IOHelper.zipFiles(hFile1, hFile2);
     List<HelperFile> unzipFiles = IOHelper.unzipFiles(new HelperFile(zipFiles, "This is the zip"));
     for (HelperFile helperFile : unzipFiles) {
@@ -71,22 +194,22 @@ public class TestClass {
     HelperFile hFile1 = new HelperFile(file1);
     HelperFile hFile2 = new HelperFile(file2);
 
-    Date start1 = new Date();
+    long start1 = System.nanoTime();
     for (int i = 0; i < 10; i++) {
       IOHelper.zipFiles(new File(IOHelper.homeDir + "\\Test With Files.zip"), file1, file2);
     }
-    Date end1 = new Date();
-    long diff1 = end1.getTime() - start1.getTime();
+    long end1 = System.nanoTime();
+    long diff1 = end1 - start1;
     System.out.println("Test with files time: " + diff1);
 
 
-    Date start2 = new Date();
+    long start2 = System.nanoTime();
     for (int i = 0; i < 10; i++) {
       byte[] zipFiles = IOHelper.zipFiles(hFile1, hFile2);
       IOHelper.saveBytesToFile(zipFiles, IOHelper.homeDir + "\\Test with Helpers.zip");
     }
-    Date end2 = new Date();
-    long diff2 = end2.getTime() - start2.getTime();
+    long end2 = System.nanoTime();
+    long diff2 = end2 - start2;
     System.out.println("Test with Helpers time: " + diff2);
     System.out.println("Time difference (diff1 - diff2): " + (diff1 - diff2));
   }
@@ -128,10 +251,14 @@ public class TestClass {
     Image resizeImage = SwingHelper.resizeImage(im, 500, 500, true);
     JLabel label = new JLabel(new ImageIcon(resizeImage));
     JDialog dialog = new JDialog();
-    dialog.setModal(true); //Important if System.exit(0) is called :)
+
+    dialog.setModal(
+            true); //Important if System.exit(0) is called :)
     dialog.getContentPane().add(label);
     SwingHelper.centerAndPack(dialog);
-    dialog.setVisible(true);
+
+    dialog.setVisible(
+            true);
   }
 
   public static void random() {
